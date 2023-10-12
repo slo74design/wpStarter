79194ab36cb1bcb87b017156d754631ee6e5dfd9
Slo74design <salvologiudice@gmail.com>
2023-10-12 10:50:52 +0100

    Installation theme
    Commit#1

---
+++ wp-content/themes/rsqStarter/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Main Template file
+ * @package rsq
+ * @author rsq
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RSQ Theme</title>
+</head>
+<body>
+    <h1>RSQ Wordpress Theme</h1>
+</body>
+</html>
